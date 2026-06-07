c024ea6b55f8d32864be3012cce09ec542c5f145
Eduardo Francisco <duudu.0304@gmail.com>
2026-06-07 16:01:36 -0300

    Endereço com objeto de valor em transportes

---
+++ PetFriends_Transporte/src/main/java/br/com/infnet/petFriends_transporte/domain/model/EnderecoEntrega.java
@@ -0,0 +1,32 @@
+package br.com.infnet.petFriends_transporte.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Embeddable
+@NoArgsConstructor
+@AllArgsConstructor
+public class EnderecoEntrega {
+
+    @Column(nullable = false, length = 150)
+    private String logradouro;
+
+    @Column(nullable = false, length = 20)
+    private String numero;
+
+    @Column(length = 50)
+    private String complemento;
+
+    @Column(nullable = false, length = 100)
+    private String cidade;
+
+    @Column(nullable = false, length = 2)
+    private String estado;
+
+    @Column(nullable = false, length = 9)
+    private String cep;
+}
